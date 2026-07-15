17af9650b5a8385dc77d943c24e9842f463fa339
First Last <me@example.com>
2026-07-15 10:54:29 +0300

    section staticits done

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ function scrollFun() {
 window.onscroll = function() {
     scrollFun()
 };
+
+
+
+window.onload = function(){
+    let website = new CountUp('webID',0,250,0,2.5)
+    website.start();
+    let phone = new CountUp('phID',0,100,0,2.5)
+    phone.start();
+    let user = new CountUp('userID',0,10000,0,2.5)
+    user.start();
+    let video = new CountUp('vidID',0,30,0,2.5)
+    video.start();
+    
+}
